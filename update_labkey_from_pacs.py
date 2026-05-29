e6a92515dd1f8b99cee3ed88bbc1b1746c37914b
Vojtěch-Benda <vojtech.benda@atlas.cz>
2026-05-29 10:29:53 +0200

    fix: wrong dicom dataset field assignment

--- update_labkey_from_pacs.py
+++ update_labkey_from_pacs.py
@@ -4,9 +4,11 @@ from argparse import ArgumentParser
 import pandas as pd
 from labkey.query import QueryFilter
 from pydicom import Dataset
+from pydicom.valuerep import DA
 from pynetdicom import AE
 from pynetdicom.sop_class import _QR_CLASSES
 from tqdm import tqdm
+from datetime import datetime
 
 from src.network import database, pacs
 
@@ -58,7 +60,7 @@ def select(api: database.LabkeyAPI, schema: str, query: str, columns: list[str])
         columns=",".join(columns),
         max_rows=-1,
         filter_array=[
-            QueryFilter("study_instance_uid", "", QueryFilter.Types.IS_BLANK)
+            QueryFilter("study_instance_uid", "", QueryFilter.Types.IS_NOT_BLANK)
         ],
     )
 
@@ -86,23 +88,29 @@ def select(api: database.LabkeyAPI, schema: str, query: str, columns: list[str])
         print("can't establish PACS association")
         sys.exit(-1)
 
-    for row in tqdm(raw_rows, miniters=100):
-        date, time = row["CAS_VYSETRENI"].split(" ")  # from format Y-m-d H:M:S
-        time = time.split(":")  # from H:M:S into [H, M, S] to filter by hour only
+    for row in tqdm(raw_rows):
+        labkey_date, labkey_time = row["CAS_VYSETRENI"].split(
+            " "
+        )  # from format Y-m-d H:M:S
+        labkey_time = labkey_time.split(
+            ":"
+        )  # from H:M:S into [H, M, S] to filter by hour only
 
         # extended tiem range to account for a few seconds up to few minute difference between study time on Labkey vs Pacs
         # example, Labkey: 13:59:20.000 vs Pacs: 14:00:40.000
         # need to account for time overflow 23:59 -> 24:00 - must be 00:00 instead
 
-        time_range = (int(time[0]), upper if (upper := int(time[0]) + 1) < 24 else 0)
+        # time_range = (int(time[0]), upper if (upper := int(time[0]) + 1) < 24 else 0)
         # time_range = f"{int(time[0]):02}-{int(time[0]) + 1:02}"
 
         ds = Dataset()
         ds.QueryRetrieveLevel = "STUDY"
         ds.PatientID = row["RODNE_CISLO"]
-        ds.StudyDate = date.replace("-", "")
-        ds.StudyDate = f"{time_range[0]:02}-{time_range[1]:02}"  # format as 2 digit with leading zero for hours < 10
+        ds.StudyDate = DA(datetime.strptime(labkey_date, "%d-%m-%Y").strftime("%Y%m%d"))
+        # ds.StudyTime = f"{time_range[0]:02}-{time_range[1]:02}"  # format as 2 digit with leading zero for hours < 10
         ds.ModalitiesInStudy = "CT"
+        ds.PatientSize = ""
+        ds.PatientWeight = ""
         ds.AccessionNumber = ""
         ds.StudyInstanceUID = ""
         ds.StudyDescription = ""
@@ -116,8 +124,13 @@ def select(api: database.LabkeyAPI, schema: str, query: str, columns: list[str])
         row["StudyDescription"] = [
             resp.get("StudyDescription", "n/a") for resp in success_resp
         ]
-        row["StudyDate"] = [resp.get("StudyDate", "n/a") for resp in success_resp]
-        row["StudyTime"] = [resp.get("StudyTime", "n/a") for resp in success_resp]
+        row["StudyDate"] = [
+            datetime.strptime(resp.get("StudyDate", "n/a"), "%Y%m%d").strftime(
+                "%d-%m-%Y"
+            )
+            for resp in success_resp
+        ]
+        # row["StudyTime"] = [resp.get("StudyTime", "n/a") for resp in success_resp]
 
     assoc.release()
     if assoc.is_released:
